15440c65af0ae290575cee2ce03e776a4c1d6d6b
ador <rashedulhaque111@gmail.com>
2022-03-01 03:11:20 +0600

    api connect

---
+++ phone.js
@@ -0,0 +1,5 @@
+function searchPhone() {
+    fetch('https://openapi.programming-hero.com/api/phones?search=$%7BsearchText%7D')
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
